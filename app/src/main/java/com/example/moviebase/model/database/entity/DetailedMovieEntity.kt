a9e55b536398a59ad3f66c21c7e1f8380e88eb19
ikelasov <ilias.kelasidis@uxma.com>
2020-05-03 16:31:44 +0200

    Remove unnecessary fields from DetailedMovieEntity

--- app/src/main/java/com/example/moviebase/model/database/entity/DetailedMovieEntity.kt
+++ app/src/main/java/com/example/moviebase/model/database/entity/DetailedMovieEntity.kt
@@ -2,7 +2,6 @@ package com.example.moviebase.model.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.moviebase.model.database.MovieEnums
 
 @Entity(tableName = "detailed_movies_table")
 data class DetailedMovieEntity(
@@ -11,9 +10,7 @@ data class DetailedMovieEntity(
     private val title: String,
     private val overview: String,
     private val posterPath: String,
-    private val type: MovieEnums.MovieType,
     private val releaseDate: String,
     private val voteAverage: Double,
-    private val voteCount: Int,
-    private val extraMovies: List<ExtraMoviesEntity>
+    private val voteCount: Int
 )
