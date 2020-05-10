ff38fdda88495457cab687055aed1077eb57cac3
ikelasov <ilias.kelasidis@uxma.com>
2020-05-10 20:26:28 +0200

    Implement MainViewModel and DetailedMovieViewModel interfaces

---
+++ app/src/main/java/com/example/moviebase/viewmodel/DetailedMovieViewModelImpl.kt
@@ -0,0 +1,74 @@
+package com.example.moviebase.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.moviebase.model.database.entity.DetailedMovieEntity
+import com.example.moviebase.model.database.entity.ExtraMoviesEntity
+import com.example.moviebase.model.repository.RepositoryImpl
+import com.example.moviebase.model.representationmodel.ExtraMovie
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class DetailedMovieViewModelImpl(
+    private val repository: RepositoryImpl,
+    application: Application
+) : AndroidViewModel(application), DetailedMovieViewModel {
+
+    // region Implements
+
+    override suspend fun fetchDetailedMovie(id: Int) {
+        viewModelScope.launch {
+            repository.replaceDetailedMovie(id)
+        }
+    }
+
+    override fun getDetailedMovie(): LiveData<DetailedMovieEntity> {
+        return repository.getDetailedMovie().asLiveData()
+    }
+
+    override fun getExtraMovies(): LiveData<List<ExtraMovie>> {
+        return repository.getExtraMovies().map { extraMoviesList ->
+            extraMoviesList.toRepresentationModelList()
+        }.asLiveData()
+    }
+
+    // endregion
+
+    // region Extension Functions
+
+    private fun List<ExtraMoviesEntity>.toRepresentationModelList(): List<ExtraMovie> {
+        val extraMoviesList = mutableListOf<ExtraMovie>()
+        for (movie in this) {
+            extraMoviesList.add(movie.toRepresentationModel())
+        }
+        return extraMoviesList
+    }
+
+    private fun ExtraMoviesEntity.toRepresentationModel(): ExtraMovie {
+        return ExtraMovie(
+            id = this.id,
+            title = this.title,
+            releaseDate = this.releaseDate,
+            posterPath = this.posterPath,
+            voteCount = this.voteCount,
+            voteAverage = this.voteAverage,
+            extraMovieType = this.extraMovieType,
+            clickAction = getClickAction(this.id)
+        )
+    }
+
+    // endregion
+
+    // region Private Functions
+
+    private fun getClickAction(id: Int): (() -> Unit)? {
+        // trigger navigation to detailed of the movie
+        return null
+    }
+
+    // endregion
+
+}
---
+++ app/src/main/java/com/example/moviebase/viewmodel/MainViewModelImpl.kt
@@ -0,0 +1,69 @@
+package com.example.moviebase.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.moviebase.model.database.entity.MoviesEntity
+import com.example.moviebase.model.repository.RepositoryImpl
+import com.example.moviebase.model.representationmodel.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+
+class MainViewModelImpl(
+    private val repository: RepositoryImpl,
+    application: Application
+) : AndroidViewModel(application), MainViewModel {
+
+    // region Implements
+
+    override fun fetchMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.replaceMovies()
+        }
+    }
+
+    override fun getMovies(): LiveData<List<Movie>> {
+        return repository.getMovies().map { movieList ->
+            movieList.toRepresentationModelList()
+        }.asLiveData()
+    }
+
+    // endregion
+
+    // region Extension Functions
+
+    private fun MoviesEntity.toRepresentationModel(): Movie {
+        return Movie(
+            id = this.id,
+            title = this.title,
+            posterPath = this.posterPath,
+            releaseDate = this.releaseDate,
+            voteAverage = this.voteAverage,
+            voteCount = this.voteCount,
+            type = this.type,
+            clickAction = getClickAction(this.id)
+        )
+    }
+
+    private fun List<MoviesEntity>.toRepresentationModelList(): List<Movie> {
+        val movieList = mutableListOf<Movie>()
+        for (movie in this) {
+            movieList.add(movie.toRepresentationModel())
+        }
+        return movieList
+    }
+
+    // endregion
+
+    // region Private Functions
+
+    private fun getClickAction(id: Int): (() -> Unit)? {
+        // trigger navigation to detailed screen
+        return null
+    }
+
+    // endregion
+}
